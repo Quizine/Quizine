c65ad911f11cbaac240590dff5637a9ee11c6f2f
John Cho <chojh92@gmail.com>
2020-02-04 14:37:31 -0500

    completed dashboard SQL & few business analytics SQL completed

--- server/api/tables.js
+++ server/api/tables.js
@@ -12,7 +12,7 @@ router.get('/summary', async (req, res, next) => {
     const interval = 'year'
     const revenue = await client.query(`
     SELECT
-    SUM (total)   
+    SUM (total)
     FROM orders
     WHERE "timeOfPurchase" > now() - interval '1 year'`) //make time dynamic  <------
     responseObject.revenue = parseInt(revenue.rows[0].sum)
@@ -28,7 +28,7 @@ router.get('/summary', async (req, res, next) => {
     const waiterCount = await client.query(`
     SELECT
     COUNT(*)
-    FROM waiters 
+    FROM waiters
     WHERE "updatedAt" > now() - interval '1 month' `)
     responseObject.waiterCount = parseInt(waiterCount.rows[0].count)
 
@@ -36,7 +36,7 @@ router.get('/summary', async (req, res, next) => {
     SELECT
     EXTRACT(hour from orders."timeOfPurchase") AS hours,
     ROUND( AVG (orders."numberOfGuests")) AS numberOfGuests
-    FROM ORDERS 
+    FROM ORDERS
     WHERE orders."timeOfPurchase" >= NOW() - interval '1 ${interval}'
     GROUP BY hours ORDER BY hours;
     `)
@@ -50,16 +50,32 @@ router.get('/summary', async (req, res, next) => {
 router.get('/fields', async (req, res, next) => {
   try {
     const allFields = await client.query(
-      `SELECT table_name 
-        FROM information_schema.tables 
+      `SELECT table_name
+        FROM information_schema.tables
         WHERE table_type='BASE TABLE'
         AND table_schema='public'
-        AND table_name !='Sessions' 
+        AND table_name !='Sessions'
         AND table_name !='users'
         AND table_name !='menuOrders'
         AND table_name !='restaurants'`
     )
     res.json(allFields.rows)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/businessAnalytics', async (req, res, next) => {
+  try {
+    const interval = 'year'
+    const waitersByTipPercent = await client.query(`SELECT waiters.name, ROUND (AVG (orders.tip) / AVG(orders.total) * 100) as averageTipPercentage FROM ORDERS
+    JOIN WAITERS ON orders."waiterId" = waiters.id GROUP BY waiters.name ORDER BY averageTipPercentage DESC WHERE orders."timeOfPurchase" >= NOW() - interval '1 ${interval};`)
+
+    const waitersByAvgServedDish = await client.query(`SELECT waiters.name, ROUND(SUM ("menuOrders".quantity) / 7) FROM "menuOrders"
+    JOIN ORDERS ON orders.id = "menuOrders"."orderId"
+    JOIN WAITERS ON orders."waiterId" = waiters.id
+    WHERE orders."timeOfPurchase" >= NOW() - interval '1 ${interval}'
+    GROUP BY waiters.name;`)
   } catch (error) {
     next(error)
   }
