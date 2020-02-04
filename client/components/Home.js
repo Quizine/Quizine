8c13ee33a4746035cb3a0329b86fa4b2a0e29a48
Desislava Shunturova <shunturova@gmail.com>
2020-02-04 11:21:50 -0500

    updated location of summary component

--- client/components/Home.js
+++ client/components/Home.js
@@ -7,7 +7,6 @@ export class Home extends Component {
       <div>
         <h1>Welcome to Nest Egg</h1>
         <h2>Where your restaurants become profitable</h2>
-        <Summary />
       </div>
     )
   }
