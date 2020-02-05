551339869d14b7df6550017ad4864917b77fb0f3
John Cho <chojh92@gmail.com>
2020-02-05 16:54:33 -0500

    simple customized query works

--- client/components/NewQuery.js
+++ client/components/NewQuery.js
@@ -1,35 +1,39 @@
 import React, {Component} from 'react'
 import NewQueryFilters from './NewQueryFilters'
+import {connect} from 'react-redux'
+import {getTableFields} from '../store/analyticsReducer'
 
 export class NewQuery extends Component {
   constructor() {
     super()
     this.state = {
-      tables: {},
       selectedTable: ''
     }
     this.handleChange = this.handleChange.bind(this)
   }
   //CDM to get all column names on store
-
+  componentDidMount() {
+    this.props.loadTableFields()
+  }
   handleChange(event) {
     this.setState({selectedTable: event.target.value})
   }
 
   render() {
+    const tableFields = this.props.tableFields
     return (
       <div>
         <select onChange={() => this.handleChange(event)}>
           <option>Please Select</option>
-          <option value="Menu">Menu</option>
-          <option value="Waiters">Waiters</option>
-          <option value="Orders">Orders</option>
+          <option value="menus">Menu</option>
+          <option value="waiters">Waiters</option>
+          <option value="orders">Orders</option>
         </select>
         {this.state.selectedTable ? (
           <div>
             <NewQueryFilters
               selectedTable={this.state.selectedTable}
-              columnNames={this.state.tables[this.state.selectedTable]}
+              columnNames={tableFields[this.state.selectedTable]}
             />
           </div>
         ) : null}
@@ -38,4 +42,17 @@ export class NewQuery extends Component {
   }
 }
 
-export default NewQuery
+const mapStateToProps = state => {
+  return {
+    tableFields: state.analytics.tableFields
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    loadTableFields: () => {
+      dispatch(getTableFields())
+    }
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(NewQuery)
