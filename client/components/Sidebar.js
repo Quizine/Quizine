c5aa1bd810187807c97a6cac028e2d5b18abb28e
Desislava Shunturova <shunturova@gmail.com>
2020-02-07 12:02:13 -0500

    Sidebar and userProfile updated

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {withRouter} from 'react-router-dom'
+import {connect} from 'react-redux'
 import DashboardIcon from '@material-ui/icons/Dashboard'
 import FunctionsIcon from '@material-ui/icons/Functions'
 import InsertChartOutlinedIcon from '@material-ui/icons/InsertChartOutlined'
 import SidebarNav from './SidebarNav'
 import UserProfile from './UserProfile'
+import {me} from '../store/userReducer'
 
 const pages = [
   {
