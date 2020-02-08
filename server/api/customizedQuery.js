fb3d0eca531d453b553aaf62df469170ee5ac9c9
John Cho <chojh92@gmail.com>
2020-02-08 12:36:29 -0500

    working on custom query page

--- server/api/customizedQuery.js
+++ server/api/customizedQuery.js
@@ -10,8 +10,8 @@ router.get('/', async (req, res, next) => {
   try {
     const {timeInterval, tableName, columnName} = req.query
     const newQuery = await client.query(
-      `SELECT "${columnName}" 
-        FROM ${tableName} 
+      `SELECT "${columnName}"
+        FROM ${tableName}
         WHERE ${tableName}."timeOfPurchase" >= NOW() - interval '1 ${timeInterval}'`
     )
 
@@ -21,25 +21,16 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-router.get('/fields', async (req, res, next) => {
+router.get('/:tableName', async (req, res, next) => {
   try {
-    const tables = {}
-    const menuFields = await client.query(`
+    const columns = await client.query(`
       SELECT COLUMN_NAME
       FROM INFORMATION_SCHEMA.COLUMNS
-      WHERE TABLE_NAME = 'menus'`)
-    const waiterFields = await client.query(`
-      SELECT COLUMN_NAME
-      FROM INFORMATION_SCHEMA.COLUMNS
-      WHERE TABLE_NAME = 'waiters'`)
-    const orderFields = await client.query(`
-      SELECT COLUMN_NAME
-      FROM INFORMATION_SCHEMA.COLUMNS
-      WHERE TABLE_NAME = 'orders'`)
-    tables.menus = menuFields.rows
-    tables.waiters = waiterFields.rows
-    tables.orders = orderFields.rows
-    res.json(tables)
+      WHERE TABLE_NAME = '${req.params.tableName}'
+      AND COLUMN_NAME <> 'id'
+      AND COLUMN_NAME NOT LIKE '%At'
+      AND COLUMN_NAME NOT LIKE '%Id'`)
+    res.json(columns.rows)
   } catch (error) {
     next(error)
   }
