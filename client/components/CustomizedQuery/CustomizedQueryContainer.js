a8799cfe0e273fbc0d99a0cef4aabe2043a37006
Slava Adnavorau <slava.adnavorau@gmail.com>
2020-02-11 22:15:40 -0500

    Table for customQuery done

--- client/components/CustomizedQuery/CustomizedQueryContainer.js
+++ client/components/CustomizedQuery/CustomizedQueryContainer.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
 import CustomizedQueryPage from './CustomizedQueryPage'
+import TableResults from './TableResults'
 
 export default class CustomizedQueryContainer extends Component {
   render() {
     return (
       <div className="customized-query-container">
         <CustomizedQueryPage />
+        <TableResults />
       </div>
     )
   }
---
+++ client/components/CustomizedQuery/TableResults.js
@@ -0,0 +1,84 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {CSVLink} from 'react-csv'
+import _ from 'lodash'
+
+// receives rows and fields
+const exampleData = {
+  rows: [
+    {menuName: 'lobster', mealType: 'lunch'},
+    {menuName: 'pizza', mealType: 'dinner'}
+  ],
+  fields: [
+    {
+      name: 'menuName',
+      tableID: 185347,
+      columnID: 2,
+      dataTypeID: 1043,
+      dataTypeSize: -1,
+      dataTypeModifier: 259,
+      format: 'text'
+    },
+    {
+      name: 'mealType',
+      tableID: 185347,
+      columnID: 5,
+      dataTypeID: 185340,
+      dataTypeSize: 4,
+      dataTypeModifier: -1,
+      format: 'text'
+    }
+  ]
+}
+
+export class TableResults extends Component {
+  render() {
+    const tableData = this.props.queryResult
+    const [rows, columns] = modifyEndpoint(exampleData)
+    const dataForCsv = [columns, ...rows]
+
+    return (
+      <div className="table-results-cont">
+        <div className="table-wrapper">
+          <table className="fl-table">
+            <thead>
+              <tr>
+                {columns.map((col, idx) => {
+                  return <th key={idx}>{col}</th>
+                })}
+              </tr>
+            </thead>
+            <tbody>
+              {rows.map((row, idx) => {
+                return (
+                  <tr key={idx}>
+                    {row.map((item, innerIdx) => {
+                      return <td key={innerIdx}>{item}</td>
+                    })}
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+        <button type="button" className="download-btn">
+          <CSVLink data={dataForCsv}>Download CSV</CSVLink>
+        </button>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    queryResult: state.customizedQuery.customQueryResult
+  }
+}
+
+export default connect(mapStateToProps)(TableResults)
+
+function modifyEndpoint(obj) {
+  const columns = obj.fields.map(column => _.startCase(column.name))
+  const rows = obj.rows.map(row => Object.values(row))
+  return [rows, columns]
+}
