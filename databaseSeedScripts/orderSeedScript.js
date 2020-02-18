6c498aef52dfdc1441ea95d109ef5a56dbeca533
adamvare <adam44@me.com>
2020-02-18 17:22:40 -0500

    changed counter to 10000 again

--- databaseSeedScripts/orderSeedScript.js
+++ databaseSeedScripts/orderSeedScript.js
@@ -2,7 +2,7 @@
 const {normalDistributionFunc} = require('./utilities')
 
 //DATABASE SEED SCRIPT VARIABLES:
-const orderNumber = 900
+const orderNumber = 10000
 
 //for seeding waiters
 let server = [
