57d10495fb06d34b6faab9ab0b32256114e40c1e
Desislava Shunturova <shunturova@gmail.com>
2020-04-09 21:10:02 -0400

    updated backend and reducer for revenue analytics

--- client/store/revenueAnalyticsReducer.js
+++ client/store/revenueAnalyticsReducer.js
@@ -1,84 +1,81 @@
 import axios from 'axios'
 import {object} from 'prop-types'
 
-const GET_AVG_REVENUE_GUEST_VS_DOW = 'GET_AVG_REVENUE_GUEST_VS_DOW'
-const GET_MONTHLY_REVENUE_VS_LUNCH_VS_DINNER =
-  'GET_MONTHLY_REVENUE_VS_LUNCH_VS_DINNER'
+/**
+ * ACTION TYPES
+ */
+const GET_REVENUE_QUERY_INTERVAL = 'GET_REVENUE_QUERY_INTERVAL'
+const GET_REVENUE_QUERY_DATE = 'GET_REVENUE_QUERY_DATE'
 
+/**
+ * INITIAL STATE
+ */
 const initialState = {
-  avgRevenuePerGuestVsDOW: {
-    year: [],
-    month: [],
-    week: []
-  },
-  monthlyRevenueVsLunchVsDinner: {
-    allPeriod: {},
-    oneYear: {},
-    twoYears: {}
-  }
+  revenueQueryResults: {}
 }
 
-const gotAvgRevenuePerGuestVsDOW = (results, timeInterval) => ({
-  type: GET_AVG_REVENUE_GUEST_VS_DOW,
-  results,
-  timeInterval
+/**
+ * ACTION CREATORS
+ */
+
+const gotRevenueQueryResultsInterval = results => ({
+  type: GET_REVENUE_QUERY_INTERVAL,
+  results
 })
 
-const gotMonthlyRevenueVsLunchVsDinner = (chartData, yearQty) => ({
-  type: GET_MONTHLY_REVENUE_VS_LUNCH_VS_DINNER,
-  chartData,
-  yearQty
+const gotRevenueQueryResultsDate = results => ({
+  type: GET_REVENUE_QUERY_DATE,
+  results
 })
 
-export const getAvgRevenuePerGuestVsDOW = timeInterval => async dispatch => {
+/**
+ * THUNK CREATORS
+ */
+
+export const getRevenueQueryResultsInterval = (
+  timeInterval,
+  queryTitle
+) => async dispatch => {
   try {
-    const {data} = await axios.get(
-      '/api/revenueAnalytics/avgRevenuePerGuestVsDOW',
-      {
-        params: {timeInterval}
-      }
-    )
-    dispatch(gotAvgRevenuePerGuestVsDOW(data, timeInterval))
+    const res = await axios.get(`/api/revenueAnalytics/${queryTitle}`, {
+      params: {timeInterval}
+    })
+    dispatch(gotRevenueQueryResultsInterval(res.data))
   } catch (err) {
     console.error(err)
   }
 }
 
-export const getMonthlyRevenueVsLunchVsDinner = yearQty => async dispatch => {
-  let sendYear
-  if (yearQty === 'oneYear') sendYear = '1'
-  else if (yearQty === 'twoYears') sendYear = '2'
-  else sendYear = '3'
+export const getRevenueQueryResultsDate = (
+  startDate,
+  endDate,
+  queryTitle
+) => async dispatch => {
   try {
-    const {data} = await axios.get(
-      '/api/revenueAnalytics/monthlyRevenueVsLunchVsDinner',
-      {
-        params: {year: sendYear}
-      }
-    )
-    dispatch(gotMonthlyRevenueVsLunchVsDinner(data, yearQty))
-  } catch (error) {
-    console.error(error)
+    const res = await axios.get(`/api/revenueAnalytics/${queryTitle}`, {
+      params: {startDate, endDate}
+    })
+    dispatch(gotRevenueQueryResultsDate(res.data))
+  } catch (err) {
+    console.error(err)
   }
 }
 
+/**
+ * REDUCER
+ */
+
 export default function(state = initialState, action) {
   switch (action.type) {
-    case GET_AVG_REVENUE_GUEST_VS_DOW:
+    case GET_REVENUE_QUERY_INTERVAL:
       return {
         ...state,
-        avgRevenuePerGuestVsDOW: {
-          ...state.avgRevenuePerGuestVsDOW,
-          [`${action.timeInterval}`]: action.results
-        }
+        revenueQueryResults: action.results
       }
-    case GET_MONTHLY_REVENUE_VS_LUNCH_VS_DINNER:
+    case GET_REVENUE_QUERY_DATE:
       return {
         ...state,
-        monthlyRevenueVsLunchVsDinner: {
-          ...state.monthlyRevenueVsLunchVsDinner,
-          [`${action.yearQty}`]: action.chartData
-        }
+        revenueQueryResults: action.results
       }
     default:
       return state
