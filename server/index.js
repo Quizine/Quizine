4881ac5852828aaa398c5642cf235d25981c767e
Desislava Shunturova <shunturova@gmail.com>
2020-05-29 14:51:52 -0400

    removed redux logger from production environment

--- server/index.js
+++ server/index.js
@@ -41,7 +41,9 @@ passport.deserializeUser(async (id, done) => {
 
 const createApp = () => {
   // logging middleware
-  app.use(morgan('dev'))
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+  }
 
   // body parsing middleware
   app.use(express.json())
